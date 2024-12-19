fe58099a2d8aaecbc9b605d7b5d985fc13559472
Andrew Nolan <anolan@lanl.gov>
2024-12-19 11:27:19 -0700

    Export ``__version__`` to simplify things

--- mosaic/__init__.py
+++ mosaic/__init__.py
@@ -1,9 +1,12 @@
 from mosaic import datasets
 from mosaic.descriptor import Descriptor
 from mosaic.polypcolor import polypcolor
+from mosaic.version import __version__
 
 __all__ = [
     "datasets",
     "polypcolor",
-    "Descriptor"
+    "Descriptor",
+    # Constants
+    "__version__",
 ]
